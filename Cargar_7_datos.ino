1ae86f37b3ce46f8cce2452c8121e37c28dcad8a
AbrahamOrtiz1229 <118387807+AbrahamOrtiz1229@users.noreply.github.com>
2023-04-20 17:07:01 -0600

    Código para cargar 7 datos a serial

---
+++ Cargar_7_datos.ino
@@ -0,0 +1,15 @@
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  Serial.println("Hola mundo");
+  Serial.println("Enseriooooooo");
+  Serial.println("Heisenberg");
+  Serial.println("No se que poner");
+  Serial.println("Tengo hambre");
+  Serial.println("Compren su boleto pal ensamble");
+  Serial.println("Alejandro");
+  delay(3000);
+}
